9a0382106986717080d532e6f353d826178e6ada
Ranjeet Kumar Mishra <ranjeet.mishra@delhivery.com>
2019-02-16 18:16:28 +0530

    commit third

---
+++ src/main/java/TestingPurpose.java
@@ -0,0 +1,20 @@
+public class TestingPurpose {
+    private int cosde;
+    private String message;
+
+    public int getCosde() {
+        return cosde;
+    }
+
+    public void setCosde(int cosde) {
+        this.cosde = cosde;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
